c0aad2c9b5dda7d53dfdc883ec8ef89094fbfa4d
hristian893 <56555405+hristian893@users.noreply.github.com>
2021-01-17 18:23:58 +0200

    Fixed a bug with the view

--- Jobber/resources/views/job/jobIndexSearchById.blade.php
+++ Jobber/resources/views/job/jobIndexSearchById.blade.php
@@ -4,6 +4,19 @@
 
 @section('content')
 <div class="inner">
+    @if (count($results) === 0)
+    <p class="display-4 text-center">Sorry, this job doesn't exist...</p>
+    <div class="col-m text-center mx-auto">
+        <form action="/search" method="get">
+            <div class="input-group">
+                <input type="search" name="search" class="form-control" placeholder="Search for job titles, cities or companies">
+                <span class="input-group-prepend">
+                    <button type="submit" class="btn btn-primary">Search</button>
+                </span>
+            </div>
+        </form>
+    </div>
+    @endif
     <div class="container lg">
         @foreach ($results as $classRes)
 
