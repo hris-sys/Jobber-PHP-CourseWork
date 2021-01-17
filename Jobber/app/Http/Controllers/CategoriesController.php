73abc55a2e6680123c0dc19a67e4216152664082
hristian893 <56555405+hristian893@users.noreply.github.com>
2021-01-17 18:27:04 +0200

    Added categories views

--- Jobber/app/Http/Controllers/CategoriesController.php
+++ Jobber/app/Http/Controllers/CategoriesController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesController extends Controller
 {
-    
+    public function index()
+    {
+        //Get data from DB in here and pass it to the view
+        /* $results = DB::select(DB::raw("SELECT j.title, comp.name FROM jobs as j
+        INNER JOIN companies as comp
+        ON j.company_id = comp.id")); */
+
+        return view('categories.categories', [
+            'title' => 'Here you will find your next job!'
+        ]);
+    }
 }
